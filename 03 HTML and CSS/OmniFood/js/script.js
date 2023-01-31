76b2ee7e61d185d80f1baa6eb784ddc4d57ef800
Abdullah-Diab <abdullahdiab00@gmail.com>
2023-01-31 16:52:54 +0200

    Upload Omnifood

--- 03 HTML and CSS/OmniFood/js/script.js
+++ 03 HTML and CSS/OmniFood/js/script.js
@@ -1,17 +1,15 @@
-// Set current year
+// 01 Set current year:
 const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+yearEl.textContent = new Date().getFullYear();
 
-// Make mobile navigation work
+// 02 Make mobile navigation work:
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-// Smooth scrolling animation
+// 03 Smooth scrolling animation:
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
@@ -38,46 +36,24 @@ allLinks.forEach(function (link) {
   });
 });
 
-// Sticky navigation
+// 04 Sticky navigation:
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
-
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
     }
-
     if (ent.isIntersecting === true) {
       document.body.classList.remove("sticky");
     }
   },
   {
-    // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: "-85px",
   }
 );
+
 obs.observe(sectionHeroEl);
-
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
-
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
-
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
-
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
